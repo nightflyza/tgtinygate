87e0f609405b8d477d4bd4eaa626a1665d315890
Nightfly <nightfly@email.ua>
2025-10-10 15:11:29 +0300

    htacces and testing url here

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * tgtinygate -  A tiny PHP gateway for receiving and forwarding Telegram bot webhooks to a private backend.  
  */
 
-$botHookUrl = 'https://yourbackendhere/?module=hook';
+$botHookUrl = 'http://localhost/dev/ubilling/?module=testing';
 $connectTimeout = 5;
 $timeout = 10;
 
